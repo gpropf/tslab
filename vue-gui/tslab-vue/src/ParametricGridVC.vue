a6cddfc6c0591e0fa70b2cc6ab6ed84cbf47850b
Greg Propf <gpropf@gmail.com>
2024-08-04 11:10:50 -0700

    pgGlobal --> parametricGrid.

--- vue-gui/tslab-vue/src/ParametricGridVC.vue
+++ vue-gui/tslab-vue/src/ParametricGridVC.vue
@@ -20,9 +20,9 @@ const props = defineProps<{
     programaticallyCreated: boolean
 }>()
 
-let pgGlobal = new ParametricGrid<any>(props.width, props.height, props.defaultValue);
-pgGlobal.setLocation(2, 1, 120)
-console.log(pgGlobal);
+let parametricGrid = new ParametricGrid<any>(props.width, props.height, props.defaultValue);
+parametricGrid.setLocation(2, 1, 120)
+console.log(parametricGrid);
 var viewBox=`0 0 ${props.width} ${props.height}`;
 
 
@@ -35,24 +35,24 @@ const newTask = inject<number>("newTask", 75);
 
 
 <template>
-    <button @click="count++; pgGlobal.setLocation(3, 2, count * 5)">You clicked me {{ count }} times.></button>
-    <div>WIDTH: {{ pgGlobal.width }}</div>
-    <div>HEIGHT: {{ pgGlobal.height }}</div>
+    <button @click="count++; parametricGrid.setLocation(3, 2, count * 5)">You clicked me {{ count }} times.></button>
+    <div>WIDTH: {{ parametricGrid.width }}</div>
+    <div>HEIGHT: {{ parametricGrid.height }}</div>
     <div>Click val: {{ onClickValue }}</div>
     <div v-if="programaticallyCreated">This PG was created dynamically!
         <svg :viewBox="viewBox" width="320" height="200" xmlns="http://www.w3.org/2000/svg">
-    <svg v-for="(row, y) in pgGlobal._grid" xmlns="http://www.w3.org/2000/svg">
+    <svg v-for="(row, y) in parametricGrid._grid" xmlns="http://www.w3.org/2000/svg">
             
-            <svg v-for="(cellval, x) in row"><rect @click="pgGlobal.setLocation(x, y, onClickValue.value); console.log(vizFn(onClickValue.value)); $forceUpdate()" :x="x" :y="y" width="1" height="1" :fill="vizFn(cellval).fillRGB"/></svg>
+            <svg v-for="(cellval, x) in row"><rect @click="parametricGrid.setLocation(x, y, onClickValue.value); console.log(vizFn(onClickValue.value)); $forceUpdate()" :x="x" :y="y" width="1" height="1" :fill="vizFn(cellval).fillRGB"/></svg>
         
     </svg></svg>
 </div>
     <div v-else="programaticallyCreated">This PG was NOT created dynamically!
     
     <svg :viewBox="viewBox" width="320" height="200" xmlns="http://www.w3.org/2000/svg">
-        <svg v-for="(row, y) in pgGlobal._grid" xmlns="http://www.w3.org/2000/svg">
+        <svg v-for="(row, y) in parametricGrid._grid" xmlns="http://www.w3.org/2000/svg">
             
-                <svg v-for="(cellval, x) in row"><rect @click="pgGlobal.setLocation(x, y, onClickValue); console.log(vizFn(onClickValue)); $forceUpdate()" :x="x" :y="y" width="1" height="1" :fill="vizFn(cellval).fillRGB"/></svg>
+                <svg v-for="(cellval, x) in row"><rect @click="parametricGrid.setLocation(x, y, onClickValue); console.log(vizFn(onClickValue)); $forceUpdate()" :x="x" :y="y" width="1" height="1" :fill="vizFn(cellval).fillRGB"/></svg>
             
         </svg>
     </svg>
