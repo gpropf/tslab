62d416faa379e1d9bf4b311feb13ac3819cb1c8e
Greg Propf <gpropf@gmail.com>
2024-08-24 17:38:17 -0700

    Removed some dead code.

--- vue-gui/tslab-vue/src/stores/rules.ts
+++ vue-gui/tslab-vue/src/stores/rules.ts
@@ -3,20 +3,9 @@ import { defineStore } from 'pinia'
 import { RuleGrid } from "../../../../ParametricGrid"
 
 export const useRulesStore = defineStore('rules', () => {
-  //const count = ref(0)
-  //const ruleCount = ref(0)
-  //const ruleGrids = ref([])
+  
   const ruleGridMap = ref(new Map<string, RuleGrid<any>>);
-  //const doubleCount = computed(() => count.value * 2)
-  // function increment() {
-  //   count.value++
-  // }
-  // function addRule(ruleGrid) {
-  //   ruleGrids.value.push(ruleGrid)
-  //   ruleCount.value++
-  //   console.log("Adding Rule with PG: ", ruleGrid)
-  //   console.log("There are now", ruleCount.value, "rules.")
-  // }
+  
   function setRule(id: string, pgrid: RuleGrid<any>) {
     ruleGridMap.value.set(id, pgrid)
     console.log(ruleGridMap.value)
@@ -25,8 +14,7 @@ export const useRulesStore = defineStore('rules', () => {
     const rg = ruleGridMap.value.get(id)
     console.log(rg)
     return rg
-  }
-  
+  }  
 
   return { ruleGridMap, setRule, getRule }
 })
