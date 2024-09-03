3eb105fbe84e85e0a2567d7b8b1c3f217fed15ef
Greg Propf <gpropf@gmail.com>
2024-09-02 20:44:10 -0700

    The ids are now filtered to prevent offering the grid as an option to itself for succession. This filtered array is not reactive yet however.

--- vue-gui/tslab-vue/src/components/RuleSelect.vue
+++ vue-gui/tslab-vue/src/components/RuleSelect.vue
@@ -7,15 +7,20 @@ const rules = useRulesStore();
 const { ruleGridMap, setRule, getRule, serialize, getMouseLocation, setMouseLocation, getAllRuleIds } = rules;
 
 const ruleIds: string[] = getAllRuleIds();
-
 const props = defineProps<{
     fromRuleId: string
 }>()
+//ruleGridMap.keys().filter((id) => {return id != props.fromRuleId})
+
+let keys = Array.from(ruleGridMap.keys());
+const filteredKeys = keys.filter(id => id != props.fromRuleId)
+
+
 </script>
 
 <template>
     <select>
-    <option v-for="[key, value] in ruleGridMap" :key="key">
+    <option v-for="key in filteredKeys" :key="key">
       Key: {{ key }}
     
     </option>
