01cd3683c907d249fca30a250e53f3284dd64c21
Greg Propf <gpropf@gmail.com>
2024-10-06 20:45:20 -0700

    Still playing with 'context' to revive objects from JSON.

--- vue-gui/tslab-vue/src/App.vue
+++ vue-gui/tslab-vue/src/App.vue
@@ -231,18 +231,41 @@ console.log(genObjJson)
 
 console.log("gsonFooStr: ", gsonFooStr)
 
-let revivedObj = JSON.parse(genObjJson, (key, value) => {
+let revivedObj = JSON.parse(genObjJson, (key, value, context) => {
   console.log("Revived: ", key, value);
   switch (key) {
     case "myMap":
       console.log("FOUND myMap!!!!!! ", GsonClass.mapifyObject(value))
       return GsonClass.mapifyObject(value);
       break;
+    case "singleGSFooObj":
+      if (context && context.source) {
+        let revivedGSFoo = JSON.parse(context.source, (subkey, subvalue, subcontext) => {
+          if (subkey === "fooSet") {
+            let fooSet = new Set(subvalue);
+            return fooSet;
+          }
+          return subvalue;
+        });
+        return revivedGSFoo;
+      }
+      return value;
     default:
       return value;
   }
   return value;
-})
+});
+
+const bigJSON = '{"gross_gdp": 12345678901234567890}';
+const bigObj = JSON.parse(bigJSON, (key, value, context) => {
+  if (key === "gross_gdp") {
+    // Ignore the value because it has already lost precision
+    return BigInt(context.source);
+  }
+  return value;
+});
+
+console.log("bigObj: ", bigObj)
 
 //const genObjJson = `{\n\t"__gsonClassName" : "GSTestClass",\n\t"__useJSONForKeys" : [\t"_innerObjects"],\n\t"__excludeKeys" : [],\n\t"_id" : "FOO",\n\t"_innerObjects" : [{\n\t\t"__gsonClassName" : "GSTestClass",\n\t\t"__useJSONForKeys" : [\t"_innerObjects"],\n\t\t"__excludeKeys" : [],\n\t\t"_id" : "FOO-0",\n\t\t"_innerObjects" : [],\n\t\t"singleGSFooObj" : {\n\t\t\t"__gsonClassName" : "GsonClass",\n\t\t\t"__useJSONForKeys" : [],\n\t\t\t"__excludeKeys" : [],\n\t\t\t"fooFlag" : "true",\n\t\t\t"s" : "I'm a FOO!",\n\t\t\t"myMap" : {\n\t\t\t\t"Foo" : "7"\n\t\t\t}\n\n\t\t}\n,\n\t\t"myMap" : {\n\t\t\t"Foo" : "1"\n\t\t}\n,\n\t\t"testSet" : [\t"fee",\t"fii",\t"foo",\t"fum"]\n\t}\n,{\n\t\t"__gsonClassName" : "GSTestClass",\n\t\t"__useJSONForKeys" : [\t"_innerObjects"],\n\t\t"__excludeKeys" : [],\n\t\t"_id" : "FOO-1",\n\t\t"_innerObjects" : [],\n\t\t"singleGSFooObj" : {\n\t\t\t"__gsonClassName" : "GsonClass",\n\t\t\t"__useJSONForKeys" : [],\n\t\t\t"__excludeKeys" : [],\n\t\t\t"fooFlag" : "true",\n\t\t\t"s" : "I'm a FOO!",\n\t\t\t"myMap" : {\n\t\t\t\t"Foo" : "7"\n\t\t\t}\n\n\t\t}\n,\n\t\t"myMap" : {\n\t\t\t"Foo" : "1"\n\t\t}\n,\n\t\t"testSet" : [\t"fee",\t"fii",\t"foo",\t"fum"]\n\t}\n,{\n\t\t"__gsonClassName" : "GSTestClass",\n\t\t"__useJSONForKeys" : [\t"_innerObjects"],\n\t\t"__excludeKeys" : [],\n\t\t"_id" : "FOO-2",\n\t\t"_innerObjects" : [],\n\t\t"singleGSFooObj" : {\n\t\t\t"__gsonClassName" : "GsonClass",\n\t\t\t"__useJSONForKeys" : [],\n\t\t\t"__excludeKeys" : [],\n\t\t\t"fooFlag" : "true",\n\t\t\t"s" : "I'm a FOO!",\n\t\t\t"myMap" : {\n\t\t\t\t"Foo" : "7"\n\t\t\t}\n\n\t\t}\n,\n\t\t"myMap" : {\n\t\t\t"Foo" : "1"\n\t\t}\n,\n\t\t"testSet" : [\t"fee",\t"fii",\t"foo",\t"fum"]\n\t}\n,{\n\t\t"__gsonClassName" : "GSTestClass",\n\t\t"__useJSONForKeys" : [\t"_innerObjects"],\n\t\t"__excludeKeys" : [],\n\t\t"_id" : "FOO-3",\n\t\t"_innerObjects" : [],\n\t\t"singleGSFooObj" : {\n\t\t\t"__gsonClassName" : "GsonClass",\n\t\t\t"__useJSONForKeys" : [],\n\t\t\t"__excludeKeys" : [],\n\t\t\t"fooFlag" : "true",\n\t\t\t"s" : "I'm a FOO!",\n\t\t\t"myMap" : {\n\t\t\t\t"Foo" : "7"\n\t\t\t}\n\n\t\t}\n,\n\t\t"myMap" : {\n\t\t\t"Foo" : "1"\n\t\t}\n,\n\t\t"testSet" : [\t"fee",\t"fii",\t"foo",\t"fum"]\n\t}\n,{\n\t\t"__gsonClassName" : "GSTestClass",\n\t\t"__useJSONForKeys" : [\t"_innerObjects"],\n\t\t"__excludeKeys" : [],\n\t\t"_id" : "FOO-4",\n\t\t"_innerObjects" : [],\n\t\t"singleGSFooObj" : {\n\t\t\t"__gsonClassName" : "GsonClass",\n\t\t\t"__useJSONForKeys" : [],\n\t\t\t"__excludeKeys" : [],\n\t\t\t"fooFlag" : "true",\n\t\t\t"s" : "I'm a FOO!",\n\t\t\t"myMap" : {\n\t\t\t\t"Foo" : "7"\n\t\t\t}\n\n\t\t}\n,\n\t\t"myMap" : {\n\t\t\t"Foo" : "1"\n\t\t}\n,\n\t\t"testSet" : [\t"fee",\t"fii",\t"foo",\t"fum"]\n\t}\n],\n\t"singleGSFooObj" : {\n\t\t"__gsonClassName" : "GsonClass",\n\t\t"__useJSONForKeys" : [],\n\t\t"__excludeKeys" : [],\n\t\t"fooFlag" : "true",\n\t\t"s" : "I'm a FOO!",\n\t\t"myMap" : {\n\t\t\t"Foo" : "7"\n\t\t}\n\n\t}\n,\n\t"myMap" : {\n\t\t"Foo" : "1"\n\t}\n,\n\t"testSet" : [\t"fee",\t"fii",\t"foo",\t"fum"]\n}\n`
 
