e6148f3f1c732d8159505ae6e2a096fc9e5181b4
Greg Propf <gpropf@gmail.com>
2024-11-09 21:03:26 -0800

    Deleted and reinstalled node_modules in attempt to get fs and process packages recognized by VSCode and the build script.

--- vue-gui/tslab-vue/src/scripts/movie.ts
+++ vue-gui/tslab-vue/src/scripts/movie.ts
@@ -1,2 +1,12 @@
-console.log("TEST");
+import * as fs from 'fs';
+//import process from 'process';
 
+let args = process.argv.slice(2);
+
+//console.log(args);
+
+
+let jsonText = fs.readFileSync(args[0],'utf8');
+let jsonObj = JSON.parse(jsonText);
+
+console.log(jsonObj);
