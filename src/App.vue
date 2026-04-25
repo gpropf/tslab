3df697ebef28787a028e1b089e0c3e44389a9530
Greg Propf <gpropf@gmail.com>
2026-04-24 21:23:46 -0700

    Testing changes to repo to see if it rebuilds the GH Pages site.

--- src/App.vue
+++ src/App.vue
@@ -383,7 +383,7 @@ if (typeof Worker !== "undefined") {
 export default {
   data() {
     return {
-      title: 'Pixel Reactor v3.0-ghp',
+      title: 'Pixel Reactor v3.0.1-ghp',
       pgwidth: '',
       pgheight: '',
       ruleOffset: ''
