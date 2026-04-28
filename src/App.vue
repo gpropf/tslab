b0f8bf8bbe076a1df07991f47a6dd3dccd94ca83
Greg Propf <gpropf@gmail.com>
2026-04-28 15:24:30 -0700

    Downgraded vite to 7.3.2, bumped tslab version to 3.0.3.

--- src/App.vue
+++ src/App.vue
@@ -383,7 +383,7 @@ if (typeof Worker !== "undefined") {
 export default {
   data() {
     return {
-      title: 'Pixel Reactor v3.0.2-ghp',
+      title: 'Pixel Reactor v3.0.3-ghp',
       pgwidth: '',
       pgheight: '',
       ruleOffset: ''
