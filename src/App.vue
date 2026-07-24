f3b186dee3a6b02e326a401e51fee2dc771d6d59
Greg Propf <gpropf@gmail.com>
2026-07-23 17:25:36 -0700

    Fixed https://github.com/gpropf/tslab/security/dependabot/16

--- src/App.vue
+++ src/App.vue
@@ -383,7 +383,7 @@ if (typeof Worker !== "undefined") {
 export default {
   data() {
     return {
-      title: 'Pixel Reactor v3.0.8-ghp',
+      title: 'Pixel Reactor v3.0.9-ghp',
       pgwidth: '',
       pgheight: '',
       ruleOffset: ''
