78ef8e45aabf3dfb883bd189519fb96897be314e
Greg Propf <gpropf@gmail.com>
2026-07-23 17:59:09 -0700

    Removed diff from dep and devDep when I saw that everything else had vanished from the vulnerabilites list. I think putting that in actually triggered a vulnerability.

--- src/App.vue
+++ src/App.vue
@@ -383,7 +383,7 @@ if (typeof Worker !== "undefined") {
 export default {
   data() {
     return {
-      title: 'Pixel Reactor v3.0.11-ghp',
+      title: 'Pixel Reactor v3.0.12-ghp',
       pgwidth: '',
       pgheight: '',
       ruleOffset: ''
