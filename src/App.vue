b3cfb150201eed2b33efbaffde79c031e054a5b6
Greg Propf <gpropf@gmail.com>
2026-07-26 15:43:26 -0700

    Bumped rev to 3.1.0 and got rid of the -ghp because I don't remember what it was for. I've purged all the problem deps and we're ready to merge back to main.

--- src/App.vue
+++ src/App.vue
@@ -383,7 +383,7 @@ if (typeof Worker !== "undefined") {
 export default {
   data() {
     return {
-      title: 'Pixel Reactor v3.0.15-ghp',
+      title: 'Pixel Reactor v3.1.0',
       pgwidth: '',
       pgheight: '',
       ruleOffset: ''
